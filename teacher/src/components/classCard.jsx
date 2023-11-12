46307d8a3b621e7b3035d15ca8162b7dadce2790
Chingiz <chingizof@gmail.com>
2023-11-12 17:15:03 -0500

    experimenting with imports

--- teacher/src/components/classCard.jsx
+++ teacher/src/components/classCard.jsx
@@ -13,26 +13,26 @@ const CardWrapper = styled.div`
 * grades and assignments.
 */
 
-// function ClassCard({classname}) { 
-//     return (
-//         <CardWrapper>
-//             <div className="image-container">
-//                 <img src={classname.image} alt="" />
-//             </div>
-//             <div className="classCard-info">
-//                 <h3>{classname.name}</h3>
-//                 <div className="classCard-description">
-//                     <p>{classname.description}</p>
-//                 </div>
-//             </div>
-//         </CardWrapper>
-//     )
-// }
-
-function ClassCard() {
+export const ClassCard = () => {
     return (
-        <></>
-    );
+        <CardWrapper>
+            {/* <div className="image-container">
+                <img src={classname.image} alt="" />
+            </div>
+            <div className="classCard-info">
+                <h3>{classname.name}</h3>
+                <div className="classCard-description">
+                    <p>{classname.description}</p>
+                </div>
+            </div> */}
+        </CardWrapper>
+    )
 }
 
-export default ClassCard;
+// export const ClassCard = () => {
+//     return (
+//         <></>
+//     );
+// }
+
+// export default ClassCard;
